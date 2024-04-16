e8847296a665d5494b6a6e170419aa74e1a4b520
ankurkpatel <ankur_k_patel@outlook.com>
2024-04-16 12:16:01 -0400

    header improvement

--- components/header.tsx
+++ components/header.tsx
@@ -22,7 +22,8 @@ import { ChatWindowToggle } from './chat-window-toggle'
 async function UserOrLogin() {
   const session = (await auth()) as Session
   return (
-    <>
+  
+     <>
       {session?.user ? (
         <>
           <SidebarMobile>
@@ -36,7 +37,7 @@ async function UserOrLogin() {
         </Link>
       )}
       <div className="flex items-center">
-        <IconSeparator className="size-6 text-muted-foreground/50" />
+      
         {session?.user ? (
           <UserMenu user={session.user} />
         ) : (
@@ -45,21 +46,16 @@ async function UserOrLogin() {
           </Button>
         )}
       </div>
-    </>
+      </>
   )
 }
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4  shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-      <div className="flex items-center">
-      <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
-          <UserOrLogin />
-      </React.Suspense>
-      </div>
-      <div className="flex items-center justify-end space-x-2">
-  
-      </div>
-    </header>
+        <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
+        <UserOrLogin/>
+        </React.Suspense>
+      </header>
   )
 }
