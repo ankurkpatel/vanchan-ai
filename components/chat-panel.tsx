1fc530c1810b1e1729b9d91e05fd3325d3489f15
ankurkpatel <ankur_k_patel@outlook.com>
2024-04-20 19:43:33 -0400

    increase opacity of header

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -134,7 +134,7 @@ export function ChatPanel({
           </div>
         ) : null}
 
-        <div className="space-y-4 py-4 md:py-4 px-2 sm:px-0 bg-muted/90">
+        <div className="space-y-4 py-2 md:py-4 px-2 sm:px-0 bg-muted/90">
           <PromptForm input={input} setInput={setInput} />
           </div>
       </div>
